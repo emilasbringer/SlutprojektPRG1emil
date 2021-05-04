44989a3004ef16ae92998585a801854df026527e
emilasbringer <60388760+emilasbringer@users.noreply.github.com>
2021-05-04 10:58:56 +0200

    Varibale declaration

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created 2021-04-27
- *
+ * Emil Åsbringer
  * @author
  */
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Här startas ditt program
+    public static void main(String args) {
+    int windowWidth = 1920;
+    int windowHeight = 1080;
+    int fps = 60;
+
+    int paddle1X, paddle1Y, paddle1VX, paddle1VY;
+    int paddle2X, paddle2Y, paddle2VX, paddle2VY;
+    int ballX, ballY, ballVX, ballVY;
+
     }
 }
