5be1069d70aa0688c06544e08c36091a91ea69f9
emilasbringer <60388760+emilasbringer@users.noreply.github.com>
2021-05-07 11:20:25 +0200

    Canvas and paddle + ball variable + Keylistener

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javafx.scene.input.KeyCode;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -14,26 +16,64 @@ import java.io.IOException;
  */
 
 
-public class Main {
-    public static void main(String args) {
+public class Main extends Canvas {
+    public static void main(String[] args) {
     int windowWidth = 1920;
     int windowHeight = 1080;
     int fps = 60;
     boolean isRunning = true;
-
-    int paddle1X, paddle1Y, paddle1VX, paddle1VY;
-    int paddle2X, paddle2Y, paddle2VX, paddle2VY;
-    int ballX, ballY, ballVX, ballVY;
-
+    boolean death = false;
 
     JFrame frame = new JFrame("Pong Pandemic");
-    frame.setSize(windowWidth,windowHeight);
-    frame.pack();
+    Canvas canvas = new Main();
+    frame.setSize(windowWidth, windowHeight);
+    canvas.setSize(windowWidth  ,windowHeight);
+    frame.add(canvas);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  //  frame.addKeyListener(new KL());
+    frame.addKeyListener(new KL());
     frame.setVisible(true);
+    }
+
+    int paddle1X = 20;
+    int paddle1Y = 200;
+    int paddle1VX = 0;
+    int paddle1VY = 0;
+
+    int paddle2X = 1100;
+    int paddle2Y = 200;
+    int paddle2VX = 0;
+    int paddle2VY = 0;
+
+    int ballX = 0;
+    int ballY = 10;
+    int ballVX = 4;
+    int ballVY = 4;
 
 
+    public void update() {
+        paddle1Y += paddle1VY;
+        paddle2Y += paddle2VY;
 
+    }
+
+    private static class KL implements KeyListener {
+
+
+        private Object KeyCode;
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
     }
 }
